c61fedbe09525ad779dec65cef17a7f1cc3263cf
wgriffey <wgriffey15@gmail.com>
2024-12-31 15:18:38 -0700

    Upgrade to Astro v5 with other dependency updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -24,7 +24,6 @@ if (!(env.PUBLIC_SANITY_PROJECT_ID && env.PUBLIC_SANITY_DATASET)) {
 export default defineConfig({
     site: 'https://construx-template.netlify.app',
     adapter: netlify(),
-    output: 'hybrid',
     devToolbar: {
         enabled: false,
     },
@@ -54,5 +53,8 @@ export default defineConfig({
     },
     prefetch: {
         prefetchAll: true,
+    },
+    legacy: {
+        collections: true,
     },
 });
